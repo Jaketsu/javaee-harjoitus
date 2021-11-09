d317c8ef9367ae50f6d1d5c6f71c45b470592f03
Jaakko <jake.anonyymi@gmail.com>
2021-11-09 19:46:12 +0200

    Remove unused List import

--- src/main/java/fi/ptuomaal/ping/api/PongResource.java
+++ src/main/java/fi/ptuomaal/ping/api/PongResource.java
@@ -14,7 +14,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
 
 /*
 * Tässä luokassa määritellään rest API:n /ping resourcen pathit ja saatavilla olevat HTTP komennot.
